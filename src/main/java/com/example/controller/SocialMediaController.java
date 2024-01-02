96d0de728cd1eed6be18a2592f1387db4e6f26b0
Jonathan  Torres <Jonathan.torres25@live.com>
2024-01-02 16:12:41 +0000

    New Account created still need to add exceptions

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,13 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.example.service.AccountService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +15,23 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+@RestController
+@RequestMapping("")
 public class SocialMediaController {
 
+    AccountService accountService;
+
+    public SocialMediaController(AccountService accountService){
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Account account){
+        Account newAccount = accountService.registerAccount(account);
+        if(newAccount != null){
+            return ResponseEntity.status(200).body(newAccount);
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,31 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.AccountRepository;
+
+import com.example.entity.Account;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerAccount(Account newAccount){
+        
+        if((newAccount.getUsername().isEmpty()) || newAccount.getPassword().length() < 4){
+            return null;
+        }
+
+        if(accountRepository.findByUsername(newAccount.getUsername()) == null){
+            return null;
+        }
+
+        return accountRepository.save(newAccount);
+    }
 }
